af40e9b74da6cc8d75f9df36457c26ffe2cd87b7
Cyber-cube <112477661+Cyber-cube@users.noreply.github.com>
2023-10-01 04:58:15 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs-16_x
+        pkgs.python39Packages.pip
+        pkgs.python39Packages.pip
+        pkgs.python39Packages.flask
+        pkgs.cowsay
+    ];
+}
---
+++ static/script.js
@@ -0,0 +1,28 @@
+
+
+window.onload = function() {
+  var chance = new Chance();
+
+  var id = localStorage.getItem('id');
+
+  if (id === null) {
+    id = generateId();
+    localStorage.setItem('id', id);
+    document.getElementById('id').value = id;
+    document.getElementById('id2').value = id;
+    document.getElementById('id3').value = id;
+    document.getElementById('id4').value = id;
+    
+  } else {
+     document.getElementById('id').value = id;
+    document.getElementById('id2').value = id;
+    document.getElementById('id3').value = id;
+    document.getElementById('id4').value = id;
+  }
+
+  function generateId() {
+    var newId = chance.string({ length: 16, pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' });
+     document.getElementById('id').value = newId;
+    return newId;
+  }
+};
